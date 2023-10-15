f7d7953c31a2547d8286f60f9030032f946089f1
pingguomc <141195321+pingguomc@users.noreply.github.com>
2023-10-15 08:50:29 +0800

    Add files via upload

---
+++ src/main/java/cf/redstar/scpsl/SCPSL.java
@@ -0,0 +1,17 @@
+package cf.redstar.scpsl;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SCPSL extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
