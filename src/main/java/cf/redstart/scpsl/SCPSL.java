fa2e5c55dd787493c2bd7b6f8a3836b02b67ea51
pingguomc <141195321+pingguomc@users.noreply.github.com>
2023-10-22 19:30:35 +0800

    v0.0.2 * 修改了控制台提示bug * 添加了bstats * 添加逻辑（失败） * 完善了指令

--- src/main/java/cf/redstart/scpsl/SCPSL.java
+++ src/main/java/cf/redstart/scpsl/SCPSL.java
@@ -1,5 +1,8 @@
 package cf.redstart.scpsl;
 
+import cf.redstart.scpsl.command.SCPSLCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import cf.redstart.scpsl.Metrics.Metrics;
 import java.util.Objects;
@@ -10,38 +13,39 @@ public final class SCPSL extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        //
+        //加载bstats
         Metrics metrics = new Metrics(this, 20101);
         metrics.addCustomChart(new Metrics.SimplePie("chart_id", () -> "SCPSL"));
-
         //加载配置文件
         saveDefaultConfig();
         this.saveResource("zh_CN.yml",false);
+        this.saveResource("help.uml",false);
         //读取配置信息
-        getLogger().info("§b插件当前语言文件为:"+getConfig().getString("lang"));
+        getLogger().info("插件当前语言文件为:"+getConfig().getString("lang"));
         //播放加载消息
-        getLogger().info("§b------------------------");
-        getLogger().info("§b|        SCPSL         |");
-        getLogger().info("§b|  作者: pingguomc     |");
-        getLogger().info("§b|      插件正在加载    |");
-        getLogger().info("§b------------------------");
+        getLogger().info("------------------------");
+        getLogger().info("|        SCPSL         |");
+        getLogger().info("|  作者: pingguomc     |");
+        getLogger().info("|      插件正在加载    |");
+        getLogger().info("------------------------");
         getLogger().warning("注意！本插件和其他玩法类插件不兼容，且不要将本插件在非小游戏服务器加载");
         //注册指令
+        getLogger().info("正在注册指令……");
         Objects.requireNonNull(this.getCommand("scpsl")).setExecutor(this);
-        getLogger().info("§b正在注册指令……");
+        Bukkit.getPluginCommand("scpsl").setExecutor(new SCPSLCommand());
         //注册监听器
-        getLogger().info("§b正在注册监听器……");
-        getLogger().info("§l&§插件加载成功！");
+        getLogger().info("正在注册监听器……");
+        System.out.println("&l&9 SCP:SL插件加载成功！");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("§b------------------------");
-        getLogger().info("§b|        SCPSL         |");
-        getLogger().info("§b|  作者: pingguomc     |");
-        getLogger().info("§b|      插件已经卸载    |");
-        getLogger().info("§b|         再见！       |");
-        getLogger().info("§b------------------------");
-
+        getLogger().info("------------------------");
+        getLogger().info("|        SCPSL         |");
+        getLogger().info("|  作者: pingguomc     |");
+        getLogger().info("|      插件已经卸载    |");
+        getLogger().info("|         再见！       |");
+        getLogger().info("------------------------");
+        System.out.println("&l&9 See You ！");
     }
 }
--- src/main/java/cf/redstart/scpsl/command/SCPSLCommand.java
+++ src/main/java/cf/redstart/scpsl/command/SCPSLCommand.java
@@ -1,5 +1,6 @@
 package cf.redstart.scpsl.command;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -7,7 +8,76 @@ import org.bukkit.command.CommandSender;
 public class SCPSLCommand implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 0) {               //没输入东西
+            sender.sendMessage(ChatColor.BLUE+"请输入/scpsl help查看帮助");
+            return true;
+        }else if (args.length == 1){           //输入一个东西
+            String Cmd_1 = args[0];
+            if (Cmd_1.equals("help")) {
+                sender.sendMessage(ChatColor.BLUE+"指令帮助");
+                return true;
+            }else if (Cmd_1.equals("start")){
+                sender.sendMessage(ChatColor.BLUE+"强制开始游戏");
+                return true;
+            }else if (Cmd_1.equals("over")){
+                sender.sendMessage(ChatColor.BLUE+"强行停止游戏");
+                return true;
+            }else if (Cmd_1.equals("god")){
+                sender.sendMessage("你无敌了");
+                return true;
+            }else if (Cmd_1.equals("give")){
+                sender.sendMessage(ChatColor.BLUE+"请输入/scpsl help查看帮助");
+                return true;
+            }else if (Cmd_1.equals("become")){
+                sender.sendMessage(ChatColor.BLUE+"请输入/scpsl help查看帮助");
+                return true;
+            }else if (Cmd_1.equals("alpha")){
+                sender.sendMessage(ChatColor.BLUE+"请输入/scpsl help查看帮助");
+                return true;
+            }else {
+                sender.sendMessage(ChatColor.RED+"未知指令！");
+                return true;
+            }
+        }else if(args.length == 2){                              //输入两个东西
+            String Cmd_1 = args[0];
+            String Cmd_2 = args[1];
+            if (Cmd_1.equals("give")){
+                if (Cmd_2.equals("SCP-018")){
+                    sender.sendMessage("弹力球");
+                    return true;
+                }else {
+                    sender.sendMessage(ChatColor.RED+"未知指令！");
+                    return true;
+                }
+            }else if (Cmd_1.equals("become")){
+                if (Cmd_2.equals("SCP-173")) {
+                    sender.sendMessage("你成为了SCP-173");
+                    return true;
+                }else {
+                    sender.sendMessage(ChatColor.RED+"未知指令！");
+                    return true;
+                }
+            }else if(Cmd_1.equals("alpha")){
+                if (Cmd_2.equals("open")){
+                    sender.sendMessage("开启核弹");
+                    return true;
+                }else if (Cmd_2.equals("close")){
+                    sender.sendMessage("关闭核弹");
+                    return true;
+                }else if (Cmd_2.equals("boom")){
+                    sender.sendMessage("立即引爆核弹");
+                    return true;
+                }else {
+                    sender.sendMessage(ChatColor.RED+"未知指令！");
+                }
+            }else {
+                sender.sendMessage(ChatColor.RED+"未知指令！");
+            }
+        }else {
+            sender.sendMessage(ChatColor.RED+"未知指令！");
+            return true;
+        }
         return false;
     }
 }
