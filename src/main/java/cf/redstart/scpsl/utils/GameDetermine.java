aeeb71407da07544b63a0e9e13efee7631f0fa71
pingguomc <141195321+pingguomc@users.noreply.github.com>
2023-10-22 11:05:20 +0800

    v0.0.2 * 修改了控制台提示bug * 添加了bstats * 添加逻辑（失败） * 下次在更吧，失败的更新……

---
+++ src/main/java/cf/redstart/scpsl/utils/GameDetermine.java
@@ -0,0 +1,24 @@
+package cf.redstart.scpsl.utils;
+
+
+import cf.redstart.scpsl.SCPSL;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GameDetermine extends JavaPlugin {
+
+    int player_num = getServer().getOnlinePlayers().size();
+
+    /*
+
+
+    public void gameState{
+        if (true) {
+            if (player_num>1) {
+
+            }
+        }
+    }
+
+
+    */
+}
